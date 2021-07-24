c5ddbdfbb0274b88c6cd3b19d272ea6cd35d6994
ONUR <atadogan06@gmail.com>
2021-07-25 01:57:58 +0300

    some fixes

--- setup.py
+++ setup.py
@@ -6,9 +6,16 @@ from setuptools import setup
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(name='HACON',
-version='pre-alpha',
+version='0.1.2-alpha',
 description="""Lots of cyber security tool""",
+long_description=long_description,
 url='https://githpipub.com/onuratakan/HACON',
 author='Onur Atakan ULUSOY',
 author_email='atadogan06@gmail.com',
